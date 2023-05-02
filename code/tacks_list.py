8905d22c3422d0dfd040a986b58f2b27a2991d3c
Julia Cortez <jcgaucha@gmail.com>
2023-05-02 20:31:49 -0300

    add and remove list

--- code/tacks_list.py
+++ code/tacks_list.py
@@ -2,15 +2,27 @@
 
 import json
 
-dict_lists={
-    'Nome':'Júlia',
-    'Sobrenome':'Cortez',
-    'Idade':'24 anos'
-}
+
+tasks=[]
+flag=0
+receive_option='1'
+
+while receive_option=='1':    
+
+    receive_task=input('Insira sua tarefa: ')
+    
+    tasks.append(receive_task)
+    flag+=1
+    print('Adicionar [1]     Remover [2]     Sair [3]')
+    receive_option=input('Digite o número correspondente: ')
+    
+if receive_option=='2':
+    
+    tasks.pop(len(tasks)-1)
 
 with open('official_dict.json', 'w', encoding='utf8') as outfile:
     json.dump(
-        dict_lists,
+        tasks,
         outfile,
         #commands for text alignment and validating accents
         ensure_ascii=False,
