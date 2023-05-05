88053f77fd5affad4f56694f4c71c39ec42edd8a
Julia Cortez <jcgaucha@gmail.com>
2023-05-04 22:15:25 -0300

    last version

--- code/tacks_list.py
+++ code/tacks_list.py
@@ -1,24 +1,50 @@
 #list of tasks where the user can insert, remove and re-add them, using JSON.
 
 import json
+import time
+import os
 
 
 tasks=[]
-flag=0
-receive_option='1'
 
-while receive_option=='1':    
 
-    receive_task=input('Insira sua tarefa: ')
+
+while True:    
     
-    tasks.append(receive_task)
-    flag+=1
     print('Adicionar [1]     Remover [2]     Sair [3]')
     receive_option=input('Digite o número correspondente: ')
+    print('\n')
+    if receive_option!='1' and receive_option!='2' and receive_option!='3':
+        print('Digite um dos números indicados!')
+        print('\n')
+        time.sleep(3)
+        continue
+
+
+    if receive_option=='2':
+        if len(tasks)==0:
+            print('Não há nada ainda para ser apagado...')
+            time.sleep(3)
+            os.system('cls')
+        else:
+            tasks.pop(len(tasks)-1)
+   
+    if receive_option=='1':
+        receive_task=input('Insira sua tarefa: ')
+        tasks.append(receive_task)
     
-if receive_option=='2':
+    if receive_option=='3':
+        break
     
-    tasks.pop(len(tasks)-1)
+    print('\n')  
+    print('Lista atualizada:')
+    for task in tasks:
+        print('->',task)
+
+    print('\n')    
+
+    
+
 
 with open('official_dict.json', 'w', encoding='utf8') as outfile:
     json.dump(
@@ -28,3 +54,11 @@ with open('official_dict.json', 'w', encoding='utf8') as outfile:
         ensure_ascii=False,
         indent=2,
     )
+
+with open('official_dict.json', 'r', encoding='utf8') as outfile:
+    tasks = json.load(outfile)
+    # print(pessoa)
+    # print(type(pessoa))
+
+
+        
